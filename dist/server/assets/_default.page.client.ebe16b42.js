210091cb2d33e4122e57bf7e4249cdf9408f5a32
Zack Cheng <zack_cheng@brown.edu>
2022-07-02 18:29:32 -0400

    Updated build | Jul 02, 06:29pm

---
+++ dist/server/assets/_default.page.client.ebe16b42.js
@@ -0,0 +1,2 @@
+"use strict";
+throw new Error("[vite-plugin-ssr][Wrong Usage] File /Users/zackcheng/Desktop/2022-2023 AY/Experiment/website/renderer/_default.page.client.jsx should not be loaded in Node.js");
---
+++ dist/server/pageFiles.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperties(exports, { __esModule: { value: true }, [Symbol.toStringTag]: { value: "Module" } });
+const pageFiles = {
+  ".page": { "/pages/hours.page.jsx": () => Promise.resolve().then(function() {
+    return require("./assets/hours.page.6253b87e.js");
+  }), "/pages/index.page.jsx": () => Promise.resolve().then(function() {
+    return require("./assets/index.page.7b94f7de.js");
+  }), "/pages/staff.page.jsx": () => Promise.resolve().then(function() {
+    return require("./assets/staff.page.062c8064.js");
+  }) },
+  ".page.client": { "/renderer/_default.page.client.jsx": () => Promise.resolve().then(function() {
+    return require("./assets/_default.page.client.ebe16b42.js");
+  }) },
+  ".page.server": { "/renderer/_default.page.server.jsx": () => Promise.resolve().then(function() {
+    return require("./assets/_default.page.server.08cbf38e.js");
+  }) },
+  ".page.route": {}
+};
+exports.pageFiles = pageFiles;
