2afd9f0b20ea09a638a5e86b3567aa2fb0dd166d
Zack Cheng <zack_cheng@brown.edu>
2022-07-09 03:11:33 -0400

    Holy hell, swapping to CSR is so easy in this framework

--- renderer/_default.page.client.jsx
+++ renderer/_default.page.client.jsx
@@ -1,18 +1,55 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import { getPage } from 'vite-plugin-ssr/client'
+import { useClientRouter } from 'vite-plugin-ssr/client/router'
 import { PageLayout } from '../resources/components/PageLayout'
 
-hydrate()
+const { hydrationPromise } = useClientRouter({
+  async render(pageContext) {
+    const { Page, pageProps } = pageContext
+    const page = (
+      <PageLayout>
+        <Page {...(pageProps ?? {})} />
+      </PageLayout>
+    )
+    const container = document.getElementById('root')
+    // `pageContext.isHydration` is set by `vite-plugin-ssr` and is `true` when the page
+    // is already rendered to HTML.
+    if (pageContext.isHydration) {
+      // We hydrate the first page. (Since we do SSR, the first page is already
+      // rendered to HTML and we merely have to hydrate it.)
+      ReactDOM.hydrate(page, container)
+    } else {
+      // We render a new page. (When the user navigates to a new page.)
+      ReactDOM.render(page, container)
+    }
+  },
 
-async function hydrate() {
-  const pageContext = await getPage()
-  const { Page, pageProps } = pageContext
+  // If `ensureHydration: true` then `vite-plugin-ssr` ensures that the first render is always
+  // a hydration. (In other words, the hydration process is never interrupted — even if the
+  // user clicks on a link before the hydration started. Default value: `false`.)
+  // If we use Vue, we need `ensureHydration: true` to avoid "Hydration Mismatch" errors.
+  // If we use React, we can leave `ensureHydration: false` for a slight performance improvement.
+  ensureHydration: false,
 
-  ReactDOM.hydrate(
-    <PageLayout>
-      <Page {...(pageProps ?? {})} />
-    </PageLayout>,
-    document.getElementById('root')
-  )
+  // See `Link prefetching` section below. Default value: `false`.
+  prefetchLinks: true,
+
+  // To create custom page transition animations
+  onTransitionStart,
+  onTransitionEnd,
+})
+
+hydrationPromise.then(() => {
+  console.log('Hydration finished; page is now interactive.')
+})
+
+function onTransitionStart() {
+  console.log('Page transition start')
+  // For example:
+  document.body.classList.add('page-transition')
+}
+function onTransitionEnd() {
+  console.log('Page transition end')
+  // For example:
+  document.body.classList.remove('page-transition')
 }
