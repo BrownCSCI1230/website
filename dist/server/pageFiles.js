8e8a279d350de811806b1a7ad824d5b511651b56
Zack Cheng <zack_cheng@brown.edu>
2022-07-09 03:12:11 -0400

    Updated build | Jul 09, 03:11am

--- dist/server/pageFiles.js
+++ dist/server/pageFiles.js
@@ -1,38 +1,44 @@
 "use strict";
 Object.defineProperties(exports, { __esModule: { value: true }, [Symbol.toStringTag]: { value: "Module" } });
 const pageFiles = {
-  ".page": { "/pages/code-blocks.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/code-blocks.page.e18c468c.js");
-  }), "/pages/hours.page.jsx": () => Promise.resolve().then(function() {
-    return require("./assets/hours.page.0c37b71c.js");
-  }), "/pages/index.page.jsx": () => Promise.resolve().then(function() {
-    return require("./assets/index.page.467c952c.js");
-  }), "/pages/inline-code.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/inline-code.page.bcef403e.js");
+  ".page": { "/pages/hours.page.mdx": () => Promise.resolve().then(function() {
+    return require("./assets/hours.page.486b61df.js");
+  }), "/pages/index.page.mdx": () => Promise.resolve().then(function() {
+    return require("./assets/index.page.8ecfc07b.js");
   }), "/pages/staff.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/staff.page.b67ca467.js");
+    return require("./assets/staff.page.e95f1fd0.js");
+  }), "/pages/docs/code-blocks.page.mdx": () => Promise.resolve().then(function() {
+    return require("./assets/code-blocks.page.a66e4ab3.js");
+  }), "/pages/docs/index.page.mdx": () => Promise.resolve().then(function() {
+    return require("./assets/index.page.f89430a5.js");
+  }), "/pages/docs/inline-code.page.mdx": () => Promise.resolve().then(function() {
+    return require("./assets/inline-code.page.4045c4a1.js");
+  }), "/pages/labs/index.page.mdx": () => Promise.resolve().then(function() {
+    return require("./assets/index.page.ae15c2a8.js");
+  }), "/pages/projects/index.page.mdx": () => Promise.resolve().then(function() {
+    return require("./assets/index.page.1cb9e73c.js");
   }), "/pages/labs/lab0/index.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/index.page.02456d51.js");
+    return require("./assets/index.page.23aa30a3.js");
   }), "/pages/labs/lab1/index.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/index.page.69a4203c.js");
+    return require("./assets/index.page.34d945b3.js");
   }), "/pages/labs/lab2/index.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/index.page.55485a8d.js");
+    return require("./assets/index.page.4a35cbf1.js");
   }), "/pages/labs/lab4/index.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/index.page.07a9cd8e.js");
+    return require("./assets/index.page.df12d625.js");
   }), "/pages/labs/lab8/index.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/index.page.7e6b90ef.js");
+    return require("./assets/index.page.e48e7d0b.js");
   }), "/pages/projects/raster/index.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/index.page.32c36c54.js");
+    return require("./assets/index.page.96c1dd0a.js");
   }), "/pages/projects/ray/index.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/index.page.24bca3a0.js");
+    return require("./assets/index.page.93b01175.js");
   }), "/pages/projects/realtime/index.page.mdx": () => Promise.resolve().then(function() {
-    return require("./assets/index.page.1af51b2c.js");
+    return require("./assets/index.page.14d43bf4.js");
   }) },
   ".page.client": { "/renderer/_default.page.client.jsx": () => Promise.resolve().then(function() {
     return require("./assets/_default.page.client.ebe16b42.js");
   }) },
   ".page.server": { "/renderer/_default.page.server.jsx": () => Promise.resolve().then(function() {
-    return require("./assets/_default.page.server.8cc56d6e.js");
+    return require("./assets/_default.page.server.002e352c.js");
   }) },
   ".page.route": {}
 };
