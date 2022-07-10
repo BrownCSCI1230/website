abf0973e76322580ecae9fb8a0f1711d8211e94d
Zack Cheng <zack_cheng@brown.edu>
2022-07-10 02:32:04 -0400

    Updated build | Jul 10, 02:31am

--- dist/server/pageFiles.js
+++ dist/server/pageFiles.js
@@ -40,7 +40,7 @@ const pageFiles = {
     return require("./assets/_default.page.client.ebe16b42.js");
   }) },
   ".page.server": { "/renderer/_default.page.server.jsx": () => Promise.resolve().then(function() {
-    return require("./assets/_default.page.server.25e4cb73.js");
+    return require("./assets/_default.page.server.5d562a48.js");
   }) },
   ".page.route": {}
 };
