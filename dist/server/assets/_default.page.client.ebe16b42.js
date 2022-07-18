60c13b0ae7d012da2634ac7735cf571048ac0402
Zack Cheng <zack_cheng@brown.edu>
2022-07-18 15:24:25 -0400

    Updated `vite` and `serve`, removed `server/`

--- dist/server/assets/_default.page.client.ebe16b42.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-throw new Error("[vite-plugin-ssr][Wrong Usage] File /Users/zackcheng/Desktop/2022-2023 AY/Experiment/website/renderer/_default.page.client.jsx should not be loaded in Node.js");
--- dist/server/importBuild.js
+++ dist/server/importBuild.js
@@ -1,13 +1,2 @@
-// Load this file if your server code is bundled. For example:
-//  - Cloudflare Workers needs the entire worker code to be bundled into a single file.
-//  - Vercel bundles serverless functions behind the scenes.
-
-// The path of following dependencies are normally determined dynamically at run-time; this file makes these dependencies discoverable at build-time so that bundlers are able to determine the entire dependency tree.
-
-
-const { pageFiles } = require("./pageFiles.js");
-const clientManifest = require("../client/manifest.json");
-const serverManifest = require("../server/manifest.json");
-const pluginManifest = require("../client/vite-plugin-ssr.json");
-const { __private: { importBuild } } = require("vite-plugin-ssr");
-importBuild({ pageFiles, clientManifest, serverManifest, pluginManifest });
+"use strict";
+console.warn("[vite-plugin-ssr][Warning] importBuild.js is not needed anymore; it has no effect. Remove `import 'dist/server/importBuild.js'` to remove this warning. More infos at https://vite-plugin-ssr.com/importBuild.js");
